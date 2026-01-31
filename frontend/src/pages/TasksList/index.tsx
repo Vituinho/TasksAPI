ab973ef3e4ec4c6c4540efcc92f56bc2153492d1
Vituinho <trabalhovictor531@gmail.com>
2026-01-31 18:03:10 -0300

    refactor: Apagando theme.css e decidindo migrar tudo pra Bootstrap

--- frontend/src/pages/TasksList/index.tsx
+++ frontend/src/pages/TasksList/index.tsx
@@ -1,9 +1,19 @@
+import { ListTasks } from '../../components/ListTasks';
+import './style.module.css'
+
 export function TasksList() {
     return (
         <>
-            <div>
-                <h1>Página de Listagem de Tasks</h1>
-                <a className="btn btn-primary" href="/tasks/add">Adicionar Task</a>
+            <div className="container">
+                <div className="d-flex align-items-center mb-4 mt-3">
+                    <div className="flex-grow-1 text-center">
+                        <h1 className="m-0 text-dark">Minhas Tarefas</h1>
+                    </div>
+                    <a href="/tasks/add" className="btn btn-primary me-3">
+                        Nova tarefa
+                    </a>
+                </div>
+                <ListTasks />
             </div>
         </>
     );
