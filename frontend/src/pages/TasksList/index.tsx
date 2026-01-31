d5f968770115a062d2ae5b6e3591defcc4250463
Vituinho <trabalhovictor531@gmail.com>
2026-01-31 17:23:17 -0300

    feat: Definindo como vai ficar o Front e adicionando um theme.css com cores desejadas

---
+++ frontend/src/pages/TasksList/index.tsx
@@ -0,0 +1,10 @@
+export function TasksList() {
+    return (
+        <>
+            <div>
+                <h1>Página de Listagem de Tasks</h1>
+                <a className="btn btn-primary" href="/tasks/add">Adicionar Task</a>
+            </div>
+        </>
+    );
+}
