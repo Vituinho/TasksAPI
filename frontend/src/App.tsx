e773be4cb2e49436f55b4ad327e9ff6aad915bc4
Vituinho <trabalhovictor531@gmail.com>
2026-02-02 19:28:50 -0300

    feat: Adicionando react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import { TasksList } from "./pages/TasksList";
+import { AddTask } from "./pages/AddTask";
 
 export function App() {
   return (
     <>
-      <TasksList />      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/tasks" element={<TasksList />} />
+        <Route path="/tasks/add" element={<AddTask/>} />
+      </Routes>      
+    </BrowserRouter>
     </>
   )
 }
